34fed74f4cf50e119e21385f2ac1c1952a91fd5c
lelfatta <lelfatta@gmail.com>
2023-09-28 00:47:41 -0700

    Update hp_LLM_app.py

--- hp_LLM_app.py
+++ hp_LLM_app.py
@@ -132,7 +132,7 @@ def main():
     # Top 1/3 for images
     st.write("###")
     col1 = st.columns(1)
-    with col1:
+    col1:
         st.image("First_Page_Manual.png", caption= "The User's Guide you are questioning!", use_column_width=True)
         st.markdown("This is the cover of [this User's Guide](https://github.com/lelfatta/hp_text_LLM/blob/main/Manual.pdf). Please use it to come up with your questions for the chatbot!")
      
